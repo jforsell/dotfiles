fc0530aceeab4f83f97bd046cf826ddc0cd82925
Johan Forsell <johan@forsell.name>
2019-06-01 12:26:25 +0100

    Use faster Powerlevel10k theme

--- setup/03-powerlevel9k.sh
+++ setup/03-powerlevel9k.sh
@@ -7,8 +7,11 @@ brew install powerlevel9k
 # Install Powerlevel9k theme to use with Oh My Zsh
 git -C ~/.oh-my-zsh/custom/themes/powerlevel9k pull || git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+# Install Powerlevel10k theme (compatible with Powerlevel9k but much faster)
+git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
 # Select theme in .zshrc
-sed -i '' "s/^ZSH_THEME=.*$/ZSH_THEME=powerlevel9k\/powerlevel9k/" ~/.zshrc
+sed -i '' "s/^ZSH_THEME=.*$/ZSH_THEME=powerlevel10k\/powerlevel10k/" ~/.zshrc
 
 # Install Nerd-Fonts
 brew tap homebrew/cask-fonts
