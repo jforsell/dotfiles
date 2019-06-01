98538e482bb08de11ed87ac218884fe927faa33b
Johan Forsell <johan@forsell.name>
2019-06-01 23:07:38 +0100

    Add zsh autosuggestions

--- setup/02-zsh.sh
+++ setup/02-zsh.sh
@@ -6,6 +6,10 @@ chsh -s /bin/zsh
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i '' "s/^plugins=(git)$/plugins=(git zsh-autosuggestions)/" ~/.zshrc
+
 # Symlink zsh dotfiles
 SCRIPTPATH=$(cd "$(dirname "$0")"; pwd)
 ln -s $SCRIPTPATH/../dotfiles/dot.zshenv ~/.zshenv
