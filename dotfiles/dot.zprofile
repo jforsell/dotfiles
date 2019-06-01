7f08e4a8aaaadda8e238fe98af2ac691cc5cfd95
Johan Forsell <johan@forsell.name>
2019-06-01 10:48:00 +0100

    Install rbenv

--- dotfiles/dot.zprofile
+++ dotfiles/dot.zprofile
@@ -2,3 +2,7 @@ export PATH=~/miniconda/envs/python2/bin:\
 ~/miniconda/envs/python3/bin:\
 ~/miniconda/bin:\
 $PATH
+
+# Load rbenv
+eval "$(rbenv init - zsh)"
+
---
+++ setup/06-rbenv.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install rbenv
+brew install rbenv
+
+# Install a Ruby (this version is used by a particular project I work on)
+rbenv install 2.4.2
+rbenv global 2.4.2
