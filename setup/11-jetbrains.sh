da1b7c844f9cb9b2572ee5ce4d73a8345d7147a0
Johan Forsell <johan@forsell.name>
2019-06-17 10:46:14 +0100

    Support Vim mode in IntelliJ Ultimate (repeating keys)

--- setup/11-jetbrains.sh
+++ setup/11-jetbrains.sh
@@ -2,4 +2,5 @@
 
 # Support Vim mode in Jetbrains apps
 defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
+defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.pycharm ApplePressAndHoldEnabled -bool false
