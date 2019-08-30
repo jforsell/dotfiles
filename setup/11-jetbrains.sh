53fa06de98a2e0466789e21c760d242fcb7def53
Johan Forsell <force@apple.com>
2019-08-30 14:51:27 +0100

    Reset global default first

--- setup/11-jetbrains.sh
+++ setup/11-jetbrains.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Reset global default first
+defaults delete -g ApplePressAndHoldEnabled
+
 # Support Vim mode in Jetbrains apps
 defaults write com.jetbrains.intellij.ce ApplePressAndHoldEnabled -bool false
 defaults write com.jetbrains.intellij ApplePressAndHoldEnabled -bool false
