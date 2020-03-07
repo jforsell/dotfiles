cc5e1de76faef690662169526e097326baf4e257
Johan Forsell <johan@forsell.name>
2020-03-07 16:27:39 +0000

    zsh is now default

--- setup/02-zsh.sh
+++ setup/02-zsh.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Change user's default shell to zsh (unnecessary in MacOS Catalina?)
-chsh -s /bin/zsh
-
 if [ ! -d "~/.oh-my-zsh" ]; then
   # Install Oh My Zsh
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
