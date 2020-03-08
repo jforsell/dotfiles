f4572755bcd168ef12761a539c741d6e008d2cfd
Johan Forsell <johan@forsell.name>
2020-03-08 07:59:40 +0000

    Powerlevel10k changes

--- setup/03-powerlevel10k.sh
+++ setup/03-powerlevel10k.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Install Powerlevel9k
-brew tap sambadevi/powerlevel9k
-brew install powerlevel9k
-
-# Install Powerlevel9k theme to use with Oh My Zsh
-git -C ~/.oh-my-zsh/custom/themes/powerlevel9k pull || git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
 # Install Powerlevel10k theme (compatible with Powerlevel9k but much faster)
 git -C ~/.oh-my-zsh/custom/themes/powerlevel10k pull || git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 # Select theme in .zshrc
 sed -i '' "s/^ZSH_THEME=.*$/ZSH_THEME=powerlevel10k\/powerlevel10k/" ~/.zshrc
+
+# Source Powerlevel10k config file
+grep -qF "# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh." ~/.zshrc
+if [ $? -ne 0 ]; then
+  echo "# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh." >> ~/.zshrc
+  echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
+fi
 
 # Install Nerd-Fonts
 brew tap homebrew/cask-fonts
