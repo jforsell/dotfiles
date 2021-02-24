cb098164e36dc9b23534b0af10fd336b8e2f4974
Johan Forsell <johan@forsell.name>
2021-02-24 16:49:27 +0000

    Homebrew @ Apple

--- setup/01-brew.sh
+++ setup/01-brew.sh
@@ -4,5 +4,17 @@
 if which brew > /dev/null; then
   echo "brew already installed"
 else
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # Install public Brew
+  # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  # Install Homebrew @ Apple, https://github.pie.apple.com/homebrew/brew
+  git clone https://github.pie.apple.com/homebrew/brew.git /tmp/homebrew
+  # sudo mv /tmp/homebrew /opt/homebrew # Apple Silicon
+  sudo mv /tmp/homebrew /opt/brew # Intel
+
+  # Get Apple's brew on the path
+  echo "/opt/homebrew/bin" | sudo tee /etc/paths.d/10-brew
+  echo "/opt/brew/bin" | sudo tee /etc/paths.d/20-brew
+  eval `/usr/libexec/path_helper -s`
+
 fi
